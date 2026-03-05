8069768c6c44d82d9297b5f10e95b5266e4e11d0
starkillerlol75 <starkillerlol75@gmail.com>
2026-03-05 20:12:06 +0000

    feat: ✨ Реализована корзина, исправлена авторизация

--- game-store-backend/routes/api.php
+++ game-store-backend/routes/api.php
@@ -4,26 +4,45 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// --- Аутентификация --- //
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+        // Для профиля пользователя
+        Route::put('/profile', [AuthController::class, 'updateProfile']); 
+        Route::post('/password', [AuthController::class, 'changePassword']);
+        Route::get('/my-reviews', [AuthController::class, 'myReviews']);
+    });
 });
+
+
+// --- Основные маршруты --- //
 
 // Игры
 Route::get('/games', [GameController::class, 'index']);
 Route::get('/games/{id}', [GameController::class, 'show']);
-Route::get('/genres', [GameController::class, 'getGenres']); // <-- Новый маршрут
+Route::get('/genres', [GameController::class, 'getGenres']);
 
 // Новости
 Route::get('/news', [NewsController::class, 'index']);
+
+// Корзина (требует авторизации)
+Route::middleware('auth:sanctum')->prefix('cart')->group(function () {
+    Route::get('/', [CartController::class, 'index']);
+    Route::post('/add', [CartController::class, 'add']);
+    Route::post('/update', [CartController::class, 'update']);
+    Route::post('/remove', [CartController::class, 'remove']);
+});
